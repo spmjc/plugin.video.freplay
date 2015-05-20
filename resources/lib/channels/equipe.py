5ff48a4a8e9bc158ac058660af27c2fc8c866d38
JUL1EN094 <jujul1en094@gmail.com>
2015-05-20 16:58:26 +0200

    Fixed l'Equipe.fr

--- resources/lib/channels/equipe.py
+++ resources/lib/channels/equipe.py
@@ -1,117 +1,77 @@
 #-*- coding: utf-8 -*-
-import urllib,urllib2
-import json    
-import re
+import resources.lib.utils as utils 
+import simplejson as json    
+import re, urllib
+from resources.lib import globalvar 
 
-title=['LEquipe.fr']
-img=['equipefr']
-readyForUse=True
+title       = ['L\'Equipe.fr']
+img         = ['equipefr']
+readyForUse = True
 
 def list_shows(channel,param):  
-  shows=[]
-           
-  jsonFile=urllib2.urlopen('http://iphdata.lequipe.fr/iPhoneDatas/EFR/STD/ALL/V2/navigation.json').read()
-  jsonParser= json.loads(jsonFile)
+  shows      = []           
+  jsonFile   = utils.get_webcontent('http://iphdata.lequipe.fr/iPhoneDatas/EFR/STD/ALL/V2/navigation.json')
+  jsonParser = json.loads(jsonFile)
   for menu in jsonParser['homes_general']:
     if 'url_video' in menu:
       shows.append( [channel,menu['url_video'], menu['nom'].encode('utf-8'),'','shows'] )
   for menu in jsonParser['homes_evenement']:
     if 'url_video' in menu:
-      shows.append( [channel,menu['url_video'], menu['nom'].encode('utf-8'),'','shows'] )
-    
+      shows.append( [channel,menu['url_video'], menu['nom'].encode('utf-8'),'','shows'] )    
   return shows
   
 def list_videos(channel,show_url):
-  videos=[] 
-  
-  jsonFile=urllib2.urlopen(show_url).read()
-  print show_url
-  jsonParser= json.loads(jsonFile)
+  videos     = []  
+  jsonFile   = utils.get_webcontent(show_url)
+  jsonParser = json.loads(jsonFile)
   for video in jsonParser['dernieres'] :
-    lien_dm=video['lien_dm'].replace(':','')   
-    title=video['titre'].encode('utf-8')      
-    icon=video['vignette_tablette_retina']
-    duration=video['duree']/60
-    infoLabels={ "Title": title,"Duration": duration} 
-    videos.append( [channel, lien_dm, title, icon,infoLabels,'play'] )     
-    
+    video_id   = video['lien_dm'].split('://')[1]   
+    title      = video['titre'].encode('utf-8')      
+    icon       = video['vignette_tablette_retina']
+    duration   = video['duree']/60
+    infoLabels = {"Title": title,"Duration": duration} 
+    videos.append([channel,video_id,title,icon,infoLabels,'play'])     
   return videos
 
-def getVideoURL(channel,videoId):
-  dmFile=urllib2.urlopen('http://www.dailymotion.com/embed/%s?autoplay=true' % (videoId)).read()
-  dmFile=urllib2.urlopen('http://www.dailymotion.com/embed/video/k5FqRVcNLkephybdrFt').read()
-  
-  video_url=temp(dmFile)
-  print 'temp',video_url    
-  
-  print 'http://www.dailymotion.com/embed/%s' % (videoId)
-  info = re.search(r'var info = ({.*?}),$', dmFile,re.MULTILINE)
-  info=info.group(1)
-  jsonParser= json.loads(info)
-  video_url= jsonParser['stream_h264_hd_url']
-  video_url=urllib.unquote_plus(video_url).replace("\\/", "/")
-        
-  print 'func',video_url 
-  
-  return video_url 
-
-def temp(link):
-#         if link.find('"error":') >= 0:
-#             err_title = re.compile('"title":"(.+?)"').findall(link)[0]
-#             if not err_title:
-#                 err_title = 'Content not available.'
-#             
-#             err_message = re.compile('"message":"(.+?)"').findall(link)[0]
-#             if not err_message:
-#                 err_message = 'No such video or the video has been removed due to copyright infringement issues.'
-#             
-#             raise UrlResolver.ResolverError(err_message)
-        
-  
-  dm_live = re.compile('live_rtsp_url":"(.+?)"', re.DOTALL).findall(link)
-  dm_1080p = re.compile('"stream_h264_hd1080_url":"(.+?)"', re.DOTALL).findall(link)
-  dm_720p = re.compile('"stream_h264_hd_url":"(.+?)"', re.DOTALL).findall(link)
-  dm_high = re.compile('"stream_h264_hq_url":"(.+?)"', re.DOTALL).findall(link)
-  dm_low = re.compile('"stream_h264_url":"(.+?)"', re.DOTALL).findall(link)
-  dm_low2 = re.compile('"stream_h264_ld_url":"(.+?)"', re.DOTALL).findall(link)
-  
-  videoUrl = []
-  if dm_live:
-      liveVideoUrl = urllib.unquote_plus(dm_live[0]).replace("\\/", "/")
-      liveVideoUrl = liveVideoUrl.replace("protocol=rtsp", "protocol=rtmp")
-      liveVideoUrl = self.net.http_GET(liveVideoUrl).content
-      videoUrl.append(liveVideoUrl)
-  else:
-      if dm_1080p:
-          videoUrl.append(urllib.unquote_plus(dm_1080p[0]).replace("\\/", "/"))
-          print videoUrl[len(videoUrl) - 1]
-      if dm_720p:
-          videoUrl.append(urllib.unquote_plus(dm_720p[0]).replace("\\/", "/"))
-          print videoUrl[len(videoUrl) - 1]
-      if dm_high:
-          videoUrl.append(urllib.unquote_plus(dm_high[0]).replace("\\/", "/"))
-          print videoUrl[len(videoUrl) - 1]
-      if dm_low:
-          videoUrl.append(urllib.unquote_plus(dm_low[0]).replace("\\/", "/")) 
-          print videoUrl[len(videoUrl) - 1]
-      if dm_low2:
-          videoUrl.append(urllib.unquote_plus(dm_low2[0]).replace("\\/", "/"))
-          print videoUrl[len(videoUrl) - 1]
-  
-  
-  print len(videoUrl)
-  vUrl = ''
-  vUrlsCount = len(videoUrl)
-  if vUrlsCount > 0:
-      q = 0
-      if q == '0':
-          # Highest Quality
-          vUrl = videoUrl[0]
-      elif q == '1':
-          # Medium Quality
-          vUrl = videoUrl[(int)(vUrlsCount / 2)]
-      elif q == '2':
-          # Lowest Quality
-          vUrl = videoUrl[vUrlsCount - 1]
-  
-  return vUrl  
+def getVideoURL(channel,video_id):
+    web_url  = 'http://www.dailymotion.com/embed/video/%s' % video_id
+    html     = utils.get_webcontent(web_url)    
+    if html.find('"error":') >= 0: return False    
+    dm_live  = re.compile('live_rtsp_url":"(.+?)"', re.DOTALL).findall(html)
+    dm_1080p = re.compile('"stream_h264_hd1080_url":"(.+?)"', re.DOTALL).findall(html)
+    dm_720p  = re.compile('"stream_h264_hd_url":"(.+?)"', re.DOTALL).findall(html)
+    dm_high  = re.compile('"stream_h264_hq_url":"(.+?)"', re.DOTALL).findall(html)
+    dm_low   = re.compile('"stream_h264_url":"(.+?)"', re.DOTALL).findall(html)
+    dm_low2  = re.compile('"stream_h264_ld_url":"(.+?)"', re.DOTALL).findall(html)    
+    video_urls = []        
+    if dm_live :
+        liveVideoUrl = urllib.unquote_plus(dm_live[0]).replace("\\/", "/")
+        liveVideoUrl = liveVideoUrl.replace("protocol=rtsp", "protocol=rtmp")
+        liveVideoUrl = utils.get_webcontent(liveVideoUrl)
+        video_urls.append(liveVideoUrl)
+    else:
+        if dm_1080p:
+            video_urls.append(urllib.unquote_plus(dm_1080p[0]).replace("\\/", "/"))
+        if dm_720p:
+            video_urls.append(urllib.unquote_plus(dm_720p[0]).replace("\\/", "/"))
+        if dm_high:
+            video_urls.append(urllib.unquote_plus(dm_high[0]).replace("\\/", "/"))
+        if dm_low:
+            video_urls.append(urllib.unquote_plus(dm_low[0]).replace("\\/", "/"))
+        if dm_low2:
+            video_urls.append(urllib.unquote_plus(dm_low2[0]).replace("\\/", "/"))    
+    video_url     = ''
+    video_url_len = len(video_urls)
+    if video_url_len > 0:
+        q = globalvar.ADDON.getSetting('equipefrQuality')
+        if q == '0':
+            # Highest Quality
+            video_url = video_urls[0]
+        elif q == '1':
+            # Medium Quality
+            video_url = video_urls[(int)(video_url_len / 2)]
+        elif q == '2':
+            # Lowest Quality
+            video_url = video_urls[video_url_len - 1]    
+    return video_url
+ 
