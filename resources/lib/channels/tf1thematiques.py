6ab27fb820aeaff9381b3e19c202576be2ac4621
Julien C <capestanj@gmail.com>
2015-06-03 00:59:56 -0400

    Added 4 Channels

---
+++ resources/lib/channels/tf1thematiques.py
@@ -0,0 +1,48 @@
+#-*- coding: utf-8 -*-
+import urllib2
+import re
+import CommonFunctions
+common = CommonFunctions 
+from resources.lib import utils     
+from resources.lib import globalvar
+
+title=['histoire','Ushuia TV', 'TV Breizh']
+img=['histoire','ushuaiatv','tvbreizh']
+readyForUse=True
+
+def list_shows(channel,folder):
+  shows=[]
+  for i in range(0, int(globalvar.ADDON.getSetting('tf1ThemePages'))):
+    filePath=utils.downloadCatalog('http://www.%s.fr/videos?page=%s' % (channel,i),'%s%s.html' % (channel,i),False)    
+    html=open(filePath).read().replace('\xe9', 'e').replace('\xe0', 'a')
+    
+    items=re.findall(r'src="(.*?)"/> 	</a></div>       <div class="views-field views-field-title">        <span class="field-content"><a href="(.*?)">(.*?)</a></span>', html.replace('\n', ' ').replace('\r', ''))
+    for item in items:
+      shows.append( [channel,item[1], item[2] , 'http://www.%s.fr/%s' % (channel,item[0]),'shows'] )
+      
+  return shows
+
+
+def getVideoURL(channel,idVideo):
+    VideoURL = 'http://wat.tv/get/ipad/'+idVideo+'.m3u8'
+    if globalvar.ADDON.getSetting('tf1ForceHD')=='true' and isHD(VideoURL) :
+        VideoURL += '?bwmin=2000000'
+    return VideoURL
+
+def list_videos(channel,show): 
+    
+  videos=[]                  
+  htmlFile=urllib2.urlopen('http://www.%s.fr%s' % (channel,show)).read().replace('\xe9', 'e').replace('\xe0', 'a').replace('\n', ' ').replace('\r', '')
+  
+  desc = re.findall(r'<meta name="description" content="(.*?)" />', htmlFile)[0] 
+  title = re.findall(r'<meta property="og:title" content="(.*?)" />', htmlFile)[0]
+  id = re.findall(r'//www.wat.tv/embedframe/(.*?)" frameborder="0"', htmlFile)[0].replace('?autoStart=1','')[-8:] 
+  infoLabels={ "Title": title,"Plot":desc}
+  videos.append( [channel, id, title, '',infoLabels,'play'] )
+      
+  return videos
+                    
+def isHD(VideoURL) :
+  m3u8 = utils.get_webcontent(VideoURL)
+  if '1280x720' in m3u8 : return True
+  else                  : return False
---
+++ resources/lib/channels/tntv.py
@@ -0,0 +1,49 @@
+#-*- coding: utf-8 -*-
+from resources.lib import utils   
+from resources.lib import globalvar 
+import json
+
+title=['TNTV']
+img=['tntv']
+readyForUse=True
+
+nbItems=25
+
+def list_shows(channel,param):  
+  shows=[]
+           
+  shows.append( [channel,'1', "Mag Actu".encode('utf-8'),'','shows'] ) 
+  shows.append( [channel,'2', "Culture".encode('utf-8'),'','shows'] )
+  shows.append( [channel,'3', "Sport".encode('utf-8'),'','shows'] )
+  shows.append( [channel,'4', "Divertissement".encode('utf-8'),'','shows'] )
+    
+  return shows
+  
+def list_videos(channel,show):
+  videos=[] 
+  
+  filePath=utils.downloadCatalog('http://tntv.goodbarber.com/apiv3/getItemsByCategorie/6833037/%s/1/%s/' % (show,nbItems),'tntv%s.json' % (show),False)
+  
+  jsonParser= json.loads(open(filePath).read())
+  for item in jsonParser['items'] :
+    videoId=str(item['id'])   
+    title=item['title'].encode('utf-8')      
+    icon=item['smallThumbnail'].encode('utf-8')
+    desc=item['summary'].encode('utf-8')
+    duration=item['length']/60
+    infoLabels={ "Title": title,"Plot":desc,"Duration": duration} 
+    videos.append( [channel, videoId, title, icon,infoLabels,'play'] )     
+    
+  return videos
+  
+def getVideoURL(channel,video_id):
+  urlVideo=''
+  filePath=utils.downloadCatalog('http://tntv.goodbarber.com/apiv3/getLastItems/6833037/1/%s/' % (nbItems*4),'tntvall.json',False)
+  
+  jsonParser= json.loads(open(filePath).read())
+  for item in jsonParser['items'] :
+    if video_id==str(item['id']):
+      print globalvar.ADDON.getSetting('tntvQuality')
+      urlVideo=item['videoUrls'][globalvar.ADDON.getSetting('tntvQuality')]
+         
+  return urlVideo
