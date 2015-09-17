4f620005a3505f52261fe696733bae67afd2e453
SPM <capestanj@gmail.com>
2015-09-16 21:01:09 -0400

    Fixed issue

--- resources/lib/channels/nrj12.py
+++ resources/lib/channels/nrj12.py
@@ -2,6 +2,7 @@
 import urllib2
 import re
 import CommonFunctions
+import base64
 common = CommonFunctions 
 from resources.lib import utils
 
@@ -41,8 +42,12 @@ def list_shows(channel,folder):
     return shows
 
 
-def getVideoURL(channel,mediaId):
-    return 'http://videos.enrj.net/web/' + mediaId + '_h264_12.mp4'
+def getVideoURL(channel,urlPage):
+
+  html=urllib2.urlopen(urlPage).read().replace('\xe9', 'e').replace('\xe0', 'a')
+  match = re.compile(r'<img class="videoPlayerContainer" data-url="(.*?)=" id="',re.DOTALL).findall(html)
+  url=base64.b64decode(match[0] + '=')
+  return url
 
 def list_videos(channel,show): 
     
@@ -65,16 +70,14 @@ def list_videos(channel,show):
                     li_s         = common.parseDOM(content_ul,"li")
                     for li in li_s :
                         image_div   = common.parseDOM(li,"div",attrs={"class":"image"}) [0]
+                        url=  re.findall(""".*href="(.*)" title.*>""",image_div) [0]
                         image_a_u   = common.parseDOM(image_div,"a") [0]
                         image_url   = re.findall(""".*src="(.*)".*>""",image_a_u) [0]
                         titre_p     = common.parseDOM(li,"p",attrs={"class":"titre"}) [0]
                         titre_u     = common.parseDOM(titre_p,"a") [0]
                         titre       = show+" : "+titre_u.encode("utf-8")
-                        date=common.parseDOM(li,"p",attrs={"class":"date"}) [0]   
-                        pos=image_url.rfind('/')+1                    
-                        media_id=image_url[pos:pos+8]
-
+                        date=common.parseDOM(li,"p",attrs={"class":"date"}) [0]
                         infoLabels={ "Title": titre,"Aired":date, "Year":date[:4]}
-                        videos.append( [channel, media_id, titre, image_url,infoLabels,'play'] )
+                        videos.append( [channel, url, titre, image_url,infoLabels,'play'] )
              
     return videos
