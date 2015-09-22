b544e2aaed8282bca025198399d50aea553348e0
JUL1EN094 <jujul1en094@gmail.com>
2015-09-22 17:26:26 +0200

    CANAL U : Fix encoding pb
    - Pb in 1st index : solved. CAUSE : the file canalu.py was encoded in ANSI instead of UTF-8 (and uft-8 coding is declared on the top of the file) ==> Changing file format to 'utf-8' solved accent and other hardencoded string.
    
    - Pb with description : solved. AS common is used to parse responses, I used these 2 modules to prettify the description :  common.replaceHTMLCodes (Replaces html codes with ascii) and common.stripTags (Removes all DOM elements)

--- resources/lib/channels/canalu.py
+++ resources/lib/channels/canalu.py
@@ -1,10 +1,10 @@
 #-*- coding: utf-8 -*-   
-import urllib2
+import urllib2, re
+from resources.lib import utils
 import CommonFunctions
 common = CommonFunctions
-from resources.lib import utils      
-import re
-
+common.plugin = "plugin.video.freplay"
+      
 title=['Canal U']
 img=['canalu']
 readyForUse=True          
@@ -13,31 +13,29 @@ def list_shows(channel,folder):
   shows=[]
   shows.append( [channel,'1','Sciences juridiques et politiques','','shows'] )
   shows.append( [channel,'2','Sciences fondamentales','','shows'] )
-  shows.append( [channel,'3','Sciences de l’ingénieur','','shows'] )
-  shows.append( [channel,'4','Sciences humaines, sociales, de l’éducation et de l’information','','shows'] )
-  shows.append( [channel,'5','Environnement et développement durable','','shows'] )
+  shows.append( [channel,'3','Sciences de lâ€™ingÃ©nieur','','shows'] )
+  shows.append( [channel,'4','Sciences humaines, sociales, de lâ€™Ã©ducation et de lâ€™information','','shows'] )
+  shows.append( [channel,'5','Environnement et dÃ©veloppement durable','','shows'] )
   shows.append( [channel,'6','Lettres, Arts, Langues et Civilisations','','shows'] )
   shows.append( [channel,'7','Economie et Gestion','','shows'] )
-  shows.append( [channel,'8','Sciences de la santé et du sport','','shows'] )
-  
+  shows.append( [channel,'8','Sciences de la santÃ© et du sport','','shows'] )
   return shows
   
 def list_videos(channel,id):
   videos=[]  
-  
   filePath=utils.downloadCatalog('http://www.canal-u.tv/themes/format.rss/theme.%s/podcast.1?xts=248546&xtor=RSS-1' % id, 'canalu%s.xml' % id,False)    
-  xml=open(filePath).read().replace('\xe9', 'e').replace('\xe0', 'a') 
+  xml=open(filePath).read() 
   items=common.parseDOM(xml, "item")
   for i in range(0, len(items)):  
     if common.parseDOM(items[i], "enclosure", ret = "type")[0]=='video/mpeg':
-      title=common.parseDOM(items[i], "title")[0].encode('utf-8')
-      description=common.parseDOM(items[i], "description")[0].encode('utf-8')
-      date=common.parseDOM(items[i], "pubDate")[0][0:16].encode('utf-8')
-      url=common.parseDOM(items[i], "link")[0]
-      
-      infoLabels={ "Title": title,"Plot":description,"Aired":date, "Year":date[:4]}
+      title          = common.parseDOM(items[i], "title")[0].encode('utf-8')
+      description    = common.parseDOM(items[i], "description")[0].encode('utf-8')
+      cleanHTML_desc = common.replaceHTMLCodes(description)
+      cleaned_desc   = common.stripTags(description)
+      date           = common.parseDOM(items[i], "pubDate")[0][0:16].encode('utf-8')
+      url            = common.parseDOM(items[i], "link")[0]      
+      infoLabels     = { "Title": title,"Plot":cleaned_desc,"Aired":date, "Year":date[:4]}
       videos.append( [channel, url, title, '',infoLabels,'play'] )
-      
       print title,url
   return videos
 
