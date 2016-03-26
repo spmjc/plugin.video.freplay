fbf127e7660fc8ceeb36d4eb4d8dcf9efab385de
Matthieu PERREIRA DA SILVA <matthieu@perreira.net>
2016-03-26 16:45:55 +0100

    Fixed gulli

--- resources/lib/channels/gulli.py
+++ resources/lib/channels/gulli.py
@@ -1,67 +1,80 @@
 #-*- coding: utf-8 -*-
 import urllib2
 import CommonFunctions
-common = CommonFunctions 
-from resources.lib import utils 
+common = CommonFunctions
+from resources.lib import utils
 import re
+from HTMLParser import HTMLParser
 
 title=['Gulli']
 img=['gulli']
 readyForUse=True
+htmlParser = HTMLParser()
 
-urlBase='http://replay.gulli.fr/replay/'
-urlVideo='http://replay.gulli.fr/replay/embed/%s?v=2.7'
+urlBase='http://replay.gulli.fr/'
+urlVideo='http://replay.gulli.fr/jwplayer/embed/%s?v=2.7'
 
 def list_shows(channel,folder):
   shows=[]
-  
-  if folder=='none': 
+
+  if folder=='none':
     shows.append( [channel,'dessins-animes','Dessins Animes','','folder'] )
     shows.append( [channel,'emissions','Emissions','','folder'] )
     shows.append( [channel,'series','Series et films','','folder'] )
-  else:           
+  else:
     d=dict()
-    filePath=utils.downloadCatalog(urlBase + folder,'gulli' + folder +'.html',False,{})    
-    html=open(filePath).read().replace('\xe9', 'e').replace('\xe0', 'a')
-    
-    replays = common.parseDOM(html,"div",attrs={"class":"img"})
-    for replay in replays :
-      title = (common.parseDOM(replay,"span",attrs={"class":"tooltip_ctnt"}) [0]).encode("utf-8") 
-      if title not in d:     
-        img = re.findall('src="(.*?)"',replay) [0]
-        shows.append( [channel,folder + '$$' + title,title,img.encode("utf-8"),'shows'] )
-        d[title]=title
-    
+    cpt=0
+    cptStr='/'
+    while cpt != -1:
+        filePath=utils.downloadCatalog(urlBase + folder + cptStr,'gulli' + folder + str(cpt) +'.html',False,{})
+        html=open(filePath).read().replace('\xe9', 'e').replace('\xe0', 'a')
+        replays = common.parseDOM(html,"div",attrs={"class":"block_category clearfix"})
+        for replay in replays :
+          title = (htmlParser.unescape(common.parseDOM(replay,"span",attrs={"class":"title"}) [0])).encode("utf-8")
+          if title not in d:
+            img = 'http://' +  re.findall('src="//(.*?)"',replay) [0]
+            shows.append( [channel,folder + '$$' + title,title,img.encode("utf-8"),'shows'] )
+            d[title]=title
+        if len(replays) != 0:
+            cpt=cpt+1
+            cptStr='/'+str(cpt)
+        else:
+            cpt=-1
   return shows
 
 def getVideoURL(channel,id):
-    html=utils.get_webcontent(urlVideo % id) 
-    url= re.findall("file: '(.*?)',",html) [0]
+    html=utils.get_webcontent(urlVideo % id)
+    url= re.findall("file: \"(.*?)\",",html) [0]
     return url
-        
+
 def list_videos(channel,param):
-  folder=param.split('$$')[0]  
+  folder=param.split('$$')[0]
   category=param.split('$$')[1]
-  
-  videos=[] 
-  filePath=utils.downloadCatalog(urlBase + folder,'gulli' + folder +'.html',False,{})    
-  html=open(filePath).read().replace('\xe9', 'e').replace('\xe0', 'a').replace("\n", " ") 
-  html=' '.join([segment for segment in html.split()])
-  
-  uls = common.parseDOM(html,"ul",attrs={"class":"liste_resultats"})
-  for ul in uls:
-    replays = common.parseDOM(ul,"li")
-    for replay in replays :
-      title = (common.parseDOM(replay,"span",attrs={"class":"tooltip_ctnt"}) [0]).encode("utf-8") 
-      if title == category:
-        match = re.compile(r'<p> <strong>(.*?)</strong> <span>(.*?)<br/>(.*?)</span> </p>',re.DOTALL).findall(replay)            
-        if match:
-          for t,st,e in match:
-            title=t + '-' + st + '-' + e.replace('&nbsp;',' ')
-        img = re.findall('src="(.*?)"',replay) [0]  
-        url= re.findall('href="(.*?)"',replay) [0]
-        iStart=url.find('VOD')
-        vodId= url[iStart:]       
-        infoLabels={ "Title": title}
-        videos.append( [channel, vodId.encode("utf-8") , title.encode("utf-8") , img,infoLabels,'play'] )
+
+  videos=[]
+  cpt=0
+  cptStr=''
+  while cpt != -1:
+    filePath=utils.downloadCatalog(urlBase + folder + cptStr,'gulli' + folder + str(cpt) +'.html',False,{})
+    html=open(filePath).read().replace('\xe9', 'e').replace('\xe0', 'a').replace("\n", " ")
+
+    uls = common.parseDOM(html,"ul",attrs={"class":"block_content clearfix"})
+    for ul in uls:
+        replays = common.parseDOM(ul,"li")
+        for replay in replays :
+          title = (common.parseDOM(replay,"span",attrs={"class":"title"}) [0]).encode("utf-8")
+          if title == category:
+            cpt=-1
+            title=(htmlParser.unescape(common.parseDOM(replay,"span",attrs={"class":"episode_title"}) [0])).encode("utf-8")
+            img = 'http://' + re.findall('src="//(.*?)"',replay) [0]
+            url= re.findall('href="(.*?)"',replay) [0]
+            iStart=url.find('VOD')
+            vodId= url[iStart:]
+            infoLabels={ "Title": title}
+            videos.append( [channel, vodId.encode("utf-8") , title , img,infoLabels,'play'] )
+    if len(uls) != 0 and cpt != -1:
+        cpt=cpt+1
+        cptStr='/'+str(cpt)
+    else:
+        cpt=-1
   return videos
