c552fa2bc283ea9212b24628e02bfaad8828a03f
Julien C <capestanj@gmail.com>
2016-05-13 23:31:05 -0400

    started rewrite of toutv

--- resources/lib/channels/toutv.py
+++ resources/lib/channels/toutv.py
@@ -7,64 +7,62 @@ title=['Tou.tv']
 img=['toutv']
 readyForUse=True          
 
+urlCatalog='http://ici.tou.tv/presentation/section/rattrapage?v=2&d=ipad&includePartnerTeaser=false'
+
+def canPlay(item):
+  if item['IsFree'] and item['IsActive'] and not item['IsGeolocalized']:
+    return True
+
 def list_shows(channel,param):  
   shows=[]
   
-  filePath=utils.downloadCatalog('http://www.tou.tv/presentation/section/a-z?AkamaiDevice=phone&smallWidth=320&mediumWidth=640&largeWidth=640&isPhone=True','TouTV.json',False,{}) 
+  filePath=utils.downloadCatalog(urlCatalog,'TouTV.json',False,{}) 
   filPrgm=open(filePath).read()
-  jsonParser     = json.loads(filPrgm)
+  jsonParser     = json.loads(filPrgm) 
+  uniqueItem = dict()  
   
-  if param=='none':
-    for menu in jsonParser['Lineups'] :
-      shows.append( [channel,menu['Name'], menu['Title'].encode('utf-8'),'','folder'] )
-  else:
-    for menu in jsonParser['Lineups'] :
-      if param==menu['Name']:
-        for item in menu['LineupItems']:
-          if item['BookmarkKey']:
-            shows.append( [channel,item['Url'], item['Title'].encode('utf-8'),item['ImageUrl'],'shows'] )  
-
+  for lineup in jsonParser['Lineups'] :
+    for lineupitem in lineup['LineupItems'] :
+      if canPlay(lineupitem):
+        if lineupitem['Title'] is not None: 
+          if lineupitem['Title'] not in uniqueItem: 
+            shows.append( [channel,lineupitem['Title'].encode('utf-8'), lineupitem['Title'].encode('utf-8'),lineupitem['ImageUrl'].encode('utf-8'),'shows'] )
+            uniqueItem[lineupitem['Title']] = lineupitem['Title']
+            
   return shows
   
-def list_videos(channel,show_URL):
-    videos=[] 
-    
-    filPrgm=urllib2.urlopen('http://www.tou.tv/presentation%s?AkamaiDevice=phone&excludeLineups=False&playerWidth=0' % (show_URL)).read()
-    jsonParser     = json.loads(filPrgm)
-    
-    url=''
-    title=jsonParser['Title'].encode('utf-8')
-    icon=''
-    
-    if jsonParser['SeasonLineups']:
-      for season in jsonParser['SeasonLineups'] :
-        for item in season['LineupItems'] :
-          url=item['Url']
-          title+='-'.encode('utf-8') + item['Title'].encode('utf-8')
-          icon=item['ImageUrl']
-          desc=item['Details']['Description'].encode('utf-8')
-          date=item['Details']['AirDate']
-          duration=item['Details']['Length']
-          
-          infoLabels={ "Title": title,"Plot":desc,"Aired":date,"Duration": duration, "Year":date[:4]}
-          videos.append( [channel, url, title, icon,infoLabels,'play'] )
-    else:
-      url=show_URL
-      title=jsonParser['Title'].encode('utf-8')
-      icon=jsonParser['ImageUrl']
-      desc=jsonParser['Details']['Description'].encode('utf-8')
-      date=jsonParser['Details']['AirDate']
-      duration=jsonParser['Details']['Length']
-      
-      infoLabels={ "Title": title,"Plot":desc,"Aired":date,"Duration": duration, "Year":date[:4]}
-      videos.append( [channel, url, title, icon,infoLabels,'play'] )
-             
-    return videos               
+def list_videos(channel,param):
+  videos=[] 
+  
+  filePath=utils.downloadCatalog(urlCatalog,'TouTV.json',False,{}) 
+  filPrgm=open(filePath).read()
+  jsonParser     = json.loads(filPrgm) 
+  
+  for lineup in jsonParser['Lineups'] :
+    for lineupitem in lineup['LineupItems'] :  
+      if canPlay(lineupitem):
+        if lineupitem['Title'] is not None: 
+          if lineupitem['Title'].encode('utf-8') == param: 
+            details=lineupitem['Details']
+            plot           = details['Description'].encode('utf-8')
+            duration   = details['Length']/60
+            titre          = param
+            if lineupitem['PromoDescription'] is not None:
+              titre+='-' + lineupitem['PromoDescription'].encode('utf-8')  
+            image      = details['ImageUrl'].encode('utf-8') 
+            url= lineupitem['Url'].encode('utf-8')
+            infoLabels = { "Title": titre,"Plot":plot,"Duration": duration}
+            videos.append( [channel, url, titre, image,infoLabels,'play'] )
+           
+  return videos               
 
 def getVideoURL(channel,video_URL):
-    filPrgm=urllib2.urlopen('http://www.tou.tv/presentation%s?AkamaiDevice=phone&excludeLineups=true&playerWidth=0' % (video_URL)).read()
+    filPrgm=urllib2.urlopen('http://ici.tou.tv/presentation%s?v=2&d=ipad&includePartnerTeaser=false' % (video_URL)).read()
     jsonParser     = json.loads(filPrgm)
-    filPrgm=urllib2.urlopen('http://api.radio-canada.ca/validationMedia/v1/Validation.html?deviceType=ipad&appCode=thePlatform&connectionType=broadband&output=json&idMedia='+ jsonParser['IdMedia']).read()
+    IdMedia=jsonParser['IdMedia']
+    IdMedia=122445
+    filPrgm=urllib2.urlopen('http://api.radio-canada.ca/ValidationMedia/v1/Validation.html?appCode=toutv&idMedia=%s&deviceType=ipad&output=json'% (IdMedia)).read()
+    print 'http://api.radio-canada.ca/ValidationMedia/v1/Validation.html?appCode=toutv&idMedia=%s&deviceType=ipad&output=json'% (IdMedia) 
     jsonParser     = json.loads(filPrgm)
     return jsonParser['url'] 
     
