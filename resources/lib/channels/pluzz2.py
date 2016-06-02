3e0898a74c1519d15adfae2bccdb63dd4724c3fa
Sylvain Cecchetto <cecchetto.sylvain@me.com>
2016-06-02 23:06:55 +0200

    Fix France Television La 1ère not working

--- resources/lib/channels/pluzz2.py
+++ resources/lib/channels/pluzz2.py
@@ -3,7 +3,7 @@ import json
 import resources.lib.utils as utils 
 from resources.lib import globalvar          
 
-title       = ['La 1ère','France 2', 'France 3', 'France 4', 'France 5', 'France O']
+title       = ['La 1ère','France 2', 'France 3', 'France 4', 'France 5', 'France Ô']
 img         = ['la_1ere' ,'france2','france3','france4','france5','franceo']
 readyForUse = True
 
@@ -13,52 +13,87 @@ imgURL         = 'http://refonte.webservices.francetelevisions.fr%s'
 
 def list_shows(channel,folder):
   shows      = []
-  uniqueItem = dict()  
-  filePath   = utils.downloadCatalog(channelCatalog % (channel),'%s.json' % (channel),False,{}) 
+  uniqueItem = dict() 
+  if channel == 'la_1ere':
+    url_json = channelCatalog % ('')
+  else:
+    url_json = channelCatalog % (channel)
+  filePath   = utils.downloadCatalog(url_json,'%s.json' % (channel),False,{}) 
   filPrgm    = open(filePath).read()
   jsonParser = json.loads(filPrgm)   
   emissions  = jsonParser['reponse']['emissions']  
   if folder=='none':           
     for emission in emissions :           
-      rubrique = nicer_rubrique_title(emission['rubrique'].encode('utf-8'))
-      if rubrique not in uniqueItem:
-        uniqueItem[rubrique] = rubrique
-        shows.append( [channel,rubrique, rubrique,'','folder'] )
+      rubrique = emission['rubrique'].encode('utf-8')
+      chaine_id = emission['chaine_id'].encode('utf-8')
+      if channel == "la_1ere":
+        if chaine_id == "la_1ere":
+          if rubrique not in uniqueItem:
+            uniqueItem[rubrique] = rubrique
+            shows.append( [channel,rubrique, change_to_nicer_name(rubrique),'','folder'] )
+          
+      else:
+        if rubrique not in uniqueItem:
+          uniqueItem[rubrique] = rubrique
+          shows.append( [channel,rubrique, change_to_nicer_name(rubrique),'','folder'] )
   else:
     for emission in emissions :           
-      rubrique = nicer_rubrique_title(emission['rubrique'].encode('utf-8'))
-      if rubrique==folder:        
-        titre = emission['titre_programme'].encode('utf-8')
-        if titre!='':      
-          id = emission['id_programme'].encode('utf-8')
-          if id=='':
-            id = emission['id_emission'].encode('utf-8')        
-          if id not in uniqueItem:
-            uniqueItem[id]=id
-            shows.append( [channel,id,titre,imgURL % (emission['image_large']),'shows'] )     
+      rubrique = emission['rubrique'].encode('utf-8')
+      chaine_id = emission['chaine_id'].encode('utf-8')
+      if channel == "la_1ere":
+        if chaine_id == "la_1ere":
+          if rubrique==folder:        
+            titre = emission['titre_programme'].encode('utf-8')
+            if titre!='':      
+              id = emission['id_programme'].encode('utf-8')
+              if id=='':
+                id = emission['id_emission'].encode('utf-8')        
+              if id not in uniqueItem:
+                uniqueItem[id]=id
+                shows.append( [channel,id,titre,imgURL % (emission['image_large']),'shows'] )   
+         
+      else:
+        if rubrique==folder:        
+          titre = emission['titre_programme'].encode('utf-8')
+          if titre!='':      
+            id = emission['id_programme'].encode('utf-8')
+            if id=='':
+              id = emission['id_emission'].encode('utf-8')        
+            if id not in uniqueItem:
+              uniqueItem[id]=id
+              shows.append( [channel,id,titre,imgURL % (emission['image_large']),'shows'] )     
   return shows
 
-def nicer_rubrique_title(rubrique):
-  print rubrique
-  if rubrique == "sport":
-    return "Sport"
-  elif rubrique == "info":
-    return "Info"
-  elif rubrique == "documentaire":
-    return "Documentaire"
-  elif rubrique == "seriefiction":
-    return "Série & fiction"
-  elif rubrique == "magazine":
-    return "Magazine"
-  elif rubrique == "jeunesse":
-    return "Jeunesse"
-  elif rubrique == "divertissement":
-    return "Divertissement"
-  elif rubrique == "jeu":
-    return "Jeu"
-  else:
-    return rubrique.title()
-  
+
+def change_to_nicer_name(original_name):
+  Dic = {"france2"              :"France 2",
+         "france3"              :"France 3",
+         "france4"              :"France 4",
+         "france5"              :"France 5",
+         "franceo"              :"France Ô",
+         "guadeloupe"           :"Guadeloupe 1ère",
+         "guyane"               :"Guyane 1ère",
+         "martinique"           :"Martinique 1ère",
+         "mayotte"              :"Mayotte 1ère",
+         "nouvellecaledonie"    :"Nouvelle Calédonie 1ère",
+         "polynesie"            :"Polynésie 1ère",
+         "reunion"              :"Réunion 1ère",
+         "saintpierreetmiquelon":"St-Pierre et Miquelon 1ère",
+         "wallisetfutuna"       :"Wallis et Futuna 1ère",
+         "sport"                :"Sport",
+         "info"                 :"Info",
+         "documentaire"         :"Documentaire",
+         "seriefiction"         :"Série & fiction",
+         "magazine"             :"Magazine",
+         "jeunesse"             :"Jeunesse",
+         "divertissement"       :"Divertissement",
+         "jeu"                  :"Jeu",
+         "culture"              :"Culture"
+         }
+  for key,value in Dic.iteritems():
+      if original_name==key : return value
+  return original_name
+
 def list_videos(channel,folder):
   videos     = []    
   uniqueItem = dict()  
