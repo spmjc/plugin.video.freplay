8a2aa2af7b4769f8991ad3c3b3f85953081d6257
Sylvain Cecchetto <cecchetto.sylvain@me.com>
2016-06-04 16:32:22 +0200

    NRJ12 - Chérie25 : Add divers infos like date, descriptions etc for shows

--- resources/lib/channels/nrj12.py
+++ resources/lib/channels/nrj12.py
@@ -20,7 +20,6 @@ def list_shows(channel,folder):
 
     if folder=='none':      
         match = re.compile(r'<li class="subNav-menu-item">(.*?)<a href="(.*?)" class=(.*?)>(.*?)</a>',re.DOTALL).findall(html)
-            
         if match:
             for empty,link,empty2,title in match:
                 if 'active' not in empty2:
@@ -31,36 +30,23 @@ def list_shows(channel,folder):
       filePath=utils.downloadCatalog('http://www.nrj-play.fr%s' % (folder),channel + folder +'.html',False,{})  
       html=open(filePath).read().replace('\xe9', 'e').replace('\xe0', 'a').replace("\n", "")
       
-      match = re.compile(r'<p class="linkProgram-quantity">(.*?)<span(.*?)<div class="linkProgram-infos">(.*?)<a href="(.*?)" class="linkProgram-thumbnail embed-responsive embed-responsive-16by9">(.*?)<img src="(.*?)" class="program-img embed-responsive-item" alt="(.*?)"',re.DOTALL).findall(html)
+      match = re.compile(r'<div class="linkProgram-infos">(.*?)<a href="(.*?)" class="linkProgram-thumbnail embed-responsive embed-responsive-16by9">(.*?)<img src="(.*?)" class="program-img embed-responsive-item" alt="(.*?)"',re.DOTALL).findall(html)
 
       if match:
-        for quantity,empty, empty1,link,empty2,img,title in match:
+        for empty1,link,empty2,img,title in match:
           title = common.replaceHTMLCodes(title)
           title = title.capitalize()
-          shows.append( [channel,link+'|'+quantity, title.encode("utf-8") , img,'shows'] )                           
+          shows.append( [channel,link, title.encode("utf-8") , img,'shows'] )                           
                      
     return shows
 
 
-def getVideoURL(channel,urlPage):
-  html=urllib2.urlopen(urlPage).read().replace('\xe9', 'e').replace('\xe0', 'a').replace('\n', 'a')
-  
-  match = re.compile(r'<link itemprop="contentUrl" href="(.*?)" />',re.DOTALL).findall(html)
-  if not match:
-    print 'hi'
-    match = re.compile(r'<meta itemprop="contentUrl" content="(.*?)" alt="',re.DOTALL).findall(html)
-  
-  url=match[0]
-  
-  
-  return url
 
 def list_videos(channel,show): 
     
     videos=[]
-    program_url = show.split('|')[0]
-    quantity = int(show.split('|')[1])
-    full_url='http://www.nrj-play.fr' + program_url
+
+    full_url='http://www.nrj-play.fr' + show
 
     opener = urllib2.build_opener()
     f = opener.open(full_url)
@@ -72,36 +58,62 @@ def list_videos(channel,show):
     base_url= full_url[:k+1]   
     print base_url
 
-    if quantity == 1:
-      match = re.compile(r'<meta itemprop="thumbnailUrl" content="(.*?)" alt="(.*?)"/>',re.DOTALL).findall(html)
-      
-      for img, title in match:
-        title = common.replaceHTMLCodes(title)
-        title = title.capitalize()        
-        infoLabels={ "Title": title}
-        videos.append( [channel, full_url, title, img,infoLabels,'play'] )           
-    
-    else:
-      match = re.compile(r'<img class="itemprop" itemprop="thumbnailUrl" src="(.*?)" alt="(.*?)" />',re.DOTALL).findall(html)
-      for img,title in match:
-        title = common.replaceHTMLCodes(title)
-        title = title.capitalize()        
+    d=dict()
+
+ 
+
+    match = re.compile(r'<img class="itemprop" itemprop="thumbnailUrl" src="(.*?)" alt="(.*?)"/>',re.DOTALL).findall(html)
+    for img,title, empty, desc in match:
+      title = common.replaceHTMLCodes(title)
+      title = title.capitalize()        
+      if title not in d:
         infoLabels={ "Title": title}
         videos.append( [channel, full_url, title, img,infoLabels,'play'] )
-      
-      match = re.compile(r'<div class="col-md-4">(.*?)<a href="(.*?)">(.*?)src="(.*?)" />(.*?)<h3><img src="(.*?)/>(.*?)</h3>',re.DOTALL).findall(html)
-      for empty,link,empty2,img,empty3,empty4,title in match:          
-        title = common.replaceHTMLCodes(title)
-        title = title.capitalize() 
-        infoLabels={ "Title": title}
-        videos.append( [channel, base_url + link, title, img,infoLabels,'play'] )
-      
-      match = re.compile(r'<div class="thumbnail-infos">(.*?)<a href="(.*?)" class="thumbnail-visual embed-responsive embed-responsive-16by9">(.*?)src="(.*?)" class="thumbnail-img embed-responsive-item" alt="(.*?)"/>',re.DOTALL).findall(html)
-      for empty,link,empty2,img,title in match:          
-        title = common.replaceHTMLCodes(title)
-        title = title.capitalize()       
-        infoLabels={ "Title": title}
-        videos.append( [channel, 'http://www.nrj-play.fr' + link, title, img,infoLabels,'play'] )  
+        d[title] = title 
     
+    # Pour les anges de NRJ12 ...
+    match = re.compile(r'<div class="col-md-4">(.*?)<a href="(.*?)">(.*?)src="(.*?)" />(.*?)Le (.*?) -(.*?)<h3><img src="(.*?)/>(.*?)</h3>',re.DOTALL).findall(html)
+    for empty,link,empty2,img,empty3, date, empty4, empty5,title in match:          
+      title = common.replaceHTMLCodes(title)
+      title = title.capitalize()
+      if title not in d:
+        infoLabels={ "Title": title, "Aired":date}
+        videos.append( [channel, base_url + link, title, img,infoLabels,'play'] )
+        d[title] = title 
+    
+    # S'il y a plusieurs vidéos pour ce programme
+    match = re.compile(r'<div class="thumbnail-infos">(.*?)<a href="(.*?)" class="thumbnail-visual embed-responsive embed-responsive-16by9">(.*?)src="(.*?)" class="thumbnail-img embed-responsive-item" alt="(.*?)"/>(.*?)class="thumbnail-time">Le (.*?)/(.*?)/(.*?)</time>',re.DOTALL).findall(html)
+    for empty,link,empty2,img,title, empty3, day, mounth, years in match:          
+      title = common.replaceHTMLCodes(title)
+      title = title.capitalize()
+      if title not in d:
+        date = day+'/'+mounth+'/'+years 
+        infoLabels={ "Title": title, "Aired":date}
+        videos.append( [channel, 'http://www.nrj-play.fr' + link, title, img,infoLabels,'play'] )
+        d[title] = title 
+
+
+    # S'il n'y a qu'une seule vidéo pour ce programme
+    match = re.compile(r'<meta itemprop="thumbnailUrl" content="(.*?)" alt="(.*?)"/>(.*?)<p>(.*?)</p>',re.DOTALL).findall(html)
+    for img, title, empty, desc in match:
+      title = common.replaceHTMLCodes(title)
+      title = title.capitalize()
+      if title not in d:
+        desc = common.replaceHTMLCodes(desc.decode('utf-8'))
+        infoLabels={ "Title": title, "Plot":desc}
+        videos.append( [channel, full_url, title, img,infoLabels,'play'] ) 
+        d[title] = title   
       
     return videos
+
+
+
+def getVideoURL(channel,urlPage):
+  html=urllib2.urlopen(urlPage).read().replace('\xe9', 'e').replace('\xe0', 'a').replace('\n', 'a')
+  
+  match = re.compile(r'<link itemprop="contentUrl" href="(.*?)" />',re.DOTALL).findall(html)
+  if not match:
+    match = re.compile(r'<meta itemprop="contentUrl" content="(.*?)" alt="',re.DOTALL).findall(html)
+  url=match[0]
+ 
+  return url
