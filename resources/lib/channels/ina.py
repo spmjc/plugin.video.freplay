b597cef39a5ee9b959f78a0e62aa37ce89c96af8
greg <greg@2600k>
2016-10-29 02:55:03 +0200

    INA: Implement list_videos

--- resources/lib/channels/ina.py
+++ resources/lib/channels/ina.py
@@ -10,7 +10,8 @@ readyForUse=True
 root_url="http://m.ina.fr"
 url_byletter=root_url + "/layout/set/ajax/listes/emissions?classObject=ina_emission&letter="
 emissions_re = re.compile(r'<a href="(.*?)"><img .* alt="(.*?)" src="(.*?)".*</a>')
-
+detail_re = re.compile(r'recherche.initialise\("(.*?)","(.*?)"\)')
+emission_json_re = re.compile(r'<h2>.*?<a href="(.*?)">(.*?)</a>', re.DOTALL)
 
 def list_shows(channel,folder):
     allshows = []
@@ -19,8 +20,32 @@ def list_shows(channel,folder):
         
     return allshows
 
-def list_videos(channel,folderIdSet):
+def list_videos(channel, emissionPage):
     print "list videos INA"    
+    
+    shows = []
+    
+    ajaxcall = getSearchUrlForEmission(channel, emissionPage)
+    filePath=utils.downloadCatalog(ajaxcall, channel+"_"+emissionPage+ ".json", False,{})    
+    raw=open(filePath).read()
+    jsoncontent=json.loads(raw)    
+    htmlcontent = jsoncontent["content"].encode("UTF-8")
+            
+    match = emission_json_re.findall(str(htmlcontent), re.DOTALL)
+    
+    if match:
+        print "match"
+        for url, title in match:
+            print "title: " + title
+            shows.append( [channel,url, title , '', {},'play'] )
+    else:
+        print "no match !"
+    
+    #TODO load image
+    #TODO info labels
+    
+    return shows
+    
 
 def getVideoURL(channel,assetId): 
     print "get video URL INA"    
@@ -46,4 +71,12 @@ def loadEmissionsForLetter(letter, channel):
     
     return shows      
  
+def getSearchUrlForEmission(channel, emissionPage):
+    emissionPage = root_url + emissionPage
+    filePath=utils.downloadCatalog(emissionPage, channel + '_details_' + emissionPage + '.html', False,{})    
+    raw=open(filePath).read()
+    
+    result = detail_re.search(raw)
+    return root_url + result.group(1) + '?' + result.group(2)
+     
       
