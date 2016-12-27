c1a9a3efa4e00ce14247102d27789afa5114a9f4
SylvainCecchetto <cecchetto.sylvain@me.com>
2016-12-27 11:08:44 +0100

    [Canal U] Nouveau script, plus de vidéos

--- resources/lib/channels/canalu.py
+++ resources/lib/channels/canalu.py
@@ -1,43 +1,91 @@
-#-*- coding: utf-8 -*-   
-import urllib2, re
+# -*- coding: utf-8 -*-
+import urllib2
+import re
 from resources.lib import utils
 import CommonFunctions
 common = CommonFunctions
 common.plugin = "plugin.video.freplay"
-      
-title=['Canal U']
-img=['canalu']
-readyForUse=True          
 
-def list_shows(channel,folder):
-  shows=[]
-  shows.append( [channel,'1','Sciences juridiques et politiques','','shows'] )
-  shows.append( [channel,'2','Sciences fondamentales','','shows'] )
-  shows.append( [channel,'3','Sciences de l’ingénieur','','shows'] )
-  shows.append( [channel,'4','Sciences humaines, sociales, de l’éducation et de l’information','','shows'] )
-  shows.append( [channel,'5','Environnement et développement durable','','shows'] )
-  shows.append( [channel,'6','Lettres, Arts, Langues et Civilisations','','shows'] )
-  shows.append( [channel,'7','Economie et Gestion','','shows'] )
-  shows.append( [channel,'8','Sciences de la santé et du sport','','shows'] )
-  return shows
-  
-def list_videos(channel,id):
-  videos=[]  
-  filePath=utils.downloadCatalog('http://www.canal-u.tv/themes/format.rss/theme.%s/podcast.1?xts=248546&xtor=RSS-1' % id, 'canalu%s.xml' % id,False,{})    
-  xml=open(filePath).read() 
-  items=common.parseDOM(xml, "item")
-  for i in range(0, len(items)):  
-    if common.parseDOM(items[i], "enclosure", ret = "type")[0]=='video/mpeg':
-      title          = common.parseDOM(items[i], "title")[0].encode('utf-8')
-      description    = common.parseDOM(items[i], "description")[0] 
-      cleaned_desc   = common.stripTags(description).replace(']]>','')
-      date           = common.parseDOM(items[i], "pubDate")[0][0:16].encode('utf-8')
-      url            = common.parseDOM(items[i], "link")[0]      
-      infoLabels     = { "Title": title,"Plot":cleaned_desc,"Aired":date, "Year":date[:4]}
-      videos.append( [channel, url, title, '',infoLabels,'play'] )
-  return videos
+title = ['Canal U']
+img = ['canalu']
+readyForUse = False
 
-def getVideoURL(channel,url):
-  html=urllib2.urlopen(url).read()
-  url = re.findall('file: "(.*?).mp4",',html) [0] + '.mp4'
-  return url
+
+def list_shows(channel, folder):
+    shows = []
+    shows.append([
+        channel,
+        '1',
+        'Sciences juridiques et politiques',
+        '',
+        'shows'])
+    shows.append([
+        channel,
+        '2',
+        'Sciences fondamentales',
+        '',
+        'shows'])
+    shows.append([
+        channel,
+        '3',
+        'Sciences de l’ingénieur',
+        '',
+        'shows'])
+    shows.append([
+        channel,
+        '4',
+        'Sciences humaines, sociales, de l’éducation et de l’information',
+        '',
+        'shows'])
+    shows.append([
+        channel,
+        '5',
+        'Environnement et développement durable',
+        '',
+        'shows'])
+    shows.append([
+        channel,
+        '6',
+        'Lettres, Arts, Langues et Civilisations',
+        '',
+        'shows'])
+    shows.append([
+        channel,
+        '7',
+        'Economie et Gestion',
+        '',
+        'shows'])
+    shows.append([
+        channel,
+        '8',
+        'Sciences de la santé et du sport',
+        '',
+        'shows'])
+    return shows
+
+
+def list_videos(channel, id):
+    videos = []
+    filePath = utils.downloadCatalog('http://www.canal-u.tv/themes/format.rss/theme.%s/podcast.1?xts=248546&xtor=RSS-1' % id, 'canalu%s.xml' % id,False,{})    
+    xml = open(filePath).read()
+    items = common.parseDOM(xml, "item")
+    for i in range(0, len(items)):
+        if common.parseDOM(items[i], "enclosure", ret="type")[0] == 'video/mpeg':
+            title = common.parseDOM(items[i], "title")[0].encode('utf-8')
+            description = common.parseDOM(items[i], "description")[0]
+            cleaned_desc = common.stripTags(description).replace(']]>', '')
+            date = common.parseDOM(items[i], "pubDate")[0][0:16].encode('utf-8')
+            url = common.parseDOM(items[i], "link")[0]
+            infoLabels = {
+                "Title": title,
+                "Plot": cleaned_desc,
+                "Aired": date,
+                "Year": date[:4]}
+            videos.append([channel, url, title, '', infoLabels, 'play'])
+    return videos
+
+
+def getVideoURL(channel, url):
+    html = urllib2.urlopen(url).read()
+    url = re.findall('file: "(.*?).mp4",', html)[0] + '.mp4'
+    return url
---
+++ resources/lib/channels/canalu2.py
@@ -0,0 +1,180 @@
+# -*- coding: utf-8 -*-
+import re
+from resources.lib import utils
+from bs4 import BeautifulSoup as bs
+
+title = ['Canal U']
+img = ['canalu']
+readyForUse = True
+
+url_root = 'http://www.canal-u.tv'
+
+
+def list_shows(channel, param):
+    shows = []
+
+    if param == 'none':
+        filePath = utils.download_catalog(
+            url_root + '/themes',
+            channel + '.html')
+        root_html = open(filePath).read()
+        root_soup = bs(root_html, "html.parser")
+
+        themes_soup = root_soup.find(
+            'div',
+            attrs={'class': 'fright'})
+        themes_soup = themes_soup.find(
+            'ul',
+            attrs={'id': 'themes'})
+
+        for theme in themes_soup.findAll('li'):
+            name_theme = theme.find('a')['title'].encode('utf-8')
+            url_theme = theme.find('a')['href'].encode('utf-8')
+            img_theme = theme.find('img')['src'].encode('utf-8')
+            depth = url_theme.count('/')
+
+            shows.append([
+                channel,
+                url_theme + '|' + str(depth),
+                name_theme,
+                url_root + img_theme,
+                'folder'])
+    else:
+        current_url = param.split('|')[0]
+        current_url_depth = int(param.split('|')[1])
+        file_path = utils.download_catalog(
+            current_url,
+            current_url + '.html')
+        theme_html = open(file_path).read()
+        theme_soup = bs(theme_html, "html.parser")
+
+        categories = theme_soup.find(
+            'ul',
+            attrs={'id': 'racine'})
+
+        categories = categories.find_all('a')
+
+        for category in categories:
+            url = category['href'].encode('utf-8')
+            url_depth = url.count('/')
+            if url_depth == current_url_depth + 1:
+                if current_url in url:
+                    title = category.find('span').get_text().encode('utf-8')
+                    next_type = 'folder'
+                    if category.find('span')['class'] == 'file':
+                        next_type = 'shows'
+
+                    shows.append([
+                        channel,
+                        url + '|' + str(url_depth),
+                        title,
+                        '',
+                        next_type])
+
+        shows.append([
+            channel,
+            current_url + '|none',
+            'Dernières vidéos de cette cétégorie',
+            '',
+            'shows'])
+
+    return shows
+
+
+def list_videos(channel, param):
+    videos = []
+    url = param.split('|')[0]
+    # print 'URL : ' + url
+    file_path = utils.download_catalog(
+        url,
+        url + '.html')
+    theme_html = open(file_path).read()
+    theme_soup = bs(theme_html, "html.parser")
+
+    videos_soup = theme_soup.find_all(
+        'li',
+        class_='fleft lasts-online-even')
+
+    videos2_soup = theme_soup.find_all(
+        'li',
+        class_='fleft lasts-online-odd')
+
+    for video in videos_soup:
+
+        url = video.find('a')['href'].encode('utf-8')
+        img = video.find('div', class_='visible')
+        img = img.find('img')['src'].encode('utf-8')
+        img = url_root + img
+        title = video.find('h4').get_text().encode('utf-8')
+        title = title.replace('\n', '').replace('\r', '')
+        duration = 0
+
+        infoLabels = {
+            "Title": title,
+            'Duration': duration}
+
+        videos.append([
+            channel,
+            url,
+            title,
+            img,
+            infoLabels,
+            'play'])
+
+    for video in videos2_soup:
+
+        url = video.find('a')['href'].encode('utf-8')
+        img = video.find('div', class_='visible')
+        img = img.find('img')['src'].encode('utf-8')
+        img = url_root + img
+        title = video.find('h4').get_text().encode('utf-8')
+        title = title.replace('\n', '').replace('\r', '')
+        duration = 0
+
+        infoLabels = {
+            "Title": title,
+            'Duration': duration}
+
+        videos.append([
+            channel,
+            url,
+            title,
+            img,
+            infoLabels,
+            'play'])
+
+    page_soup = theme_soup.find(
+        'div',
+        class_='pagination')
+    if page_soup is not None:
+        page_soup = page_soup.find_all('a')
+
+        current_page = 0
+        for page in page_soup:
+            if page.has_attr('class'):
+                if page['class'][0].encode('utf-8') == 'selected':
+                    current_page = page.get_text().encode('utf-8')
+                    current_page = int(current_page)
+
+        if current_page < len(page_soup):
+            next_url = page_soup[current_page]['href'].encode('utf-8')
+            videos.append([
+                channel,
+                next_url,
+                'Page suivante (page ' + str(current_page + 1) + ')',
+                '',
+                {},
+                'shows'])
+
+    return videos
+
+
+def getVideoURL(channel, url):
+    # print 'URL VIDEO : ' + url
+    html = utils.get_webcontent(url)
+    url_videos = re.compile(
+        r'file: "(.*?)"', re.DOTALL).findall(html)
+    for url_video in url_videos:
+        if '.mp4' in url_video:
+            return url_video
+    return url_video
