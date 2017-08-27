35ad522679f9d56d88cbccac1b60e0f53079b739
greg <gdusart@gmail.com>
2017-08-27 18:47:43 +0200

    Implement tetesaclaques.tv

--- resources/lib/channels/tetesaclaques.py
+++ resources/lib/channels/tetesaclaques.py
@@ -1,114 +1,135 @@
 #-*- coding: utf-8 -*-
-import re, json, time, threading
-from string import ascii_lowercase
-from resources.lib import utils 
-from HTMLParser import HTMLParser
-from operator import itemgetter
+from resources.lib import utils
+from bs4 import BeautifulSoup as bs
+import re
 
-title       = ['INA']
-img         = ['ina']
+
+title       = ['Têtes à claques']
+img         = ['tac']
 readyForUse = True
 
-bypass_cache = False
-lsLowercase  = list(ascii_lowercase)
+bypass_cache = True
 
-detail_re        = re.compile(r'recherche.initialise\("(.*?)","(.*?)"\)')
-emissions_re     = re.compile(r'<a href="(.*?)"><img .* alt="(.*?)" src="(.*?)".*</a>')
-emission_json_re = re.compile(r'<img .*? src="(.*?)".*?<h2>.*?<a href="(.*?)">(.*?)</a>', re.DOTALL)
-emission_url_re  = re.compile(r'<video controls src=(.*?) ', re.DOTALL)
-root_url         = "http://m.ina.fr"
-url_byletter     = root_url + "/layout/set/ajax/listes/emissions?classObject=ina_emission&letter="
+root_url         = "https://www.tetesaclaques.tv"
+categories_url     = root_url
+categories_links_selector = "#menu-videos > ul > li > a"
+clip_selector = 'div[class^=wrapCapsule]'
+video_id_re  = re.compile(r'AtedraVideo.video_id = "(.*)";')
+personnages_folder = 'personnages'
+personnages_url = root_url + '/' + personnages_folder
+personnages_link_selector = 'div.wrapThumb > a'
 
+def load_soup(url, cache_key):
+    file_path = utils.downloadCatalog(url, cache_key, bypass_cache, {})
+    html = open(file_path).read()
+    return bs(html, 'html.parser')
+
+
+#**list_shows(channel,folder)**: Utilise pour recuperer la liste des menus.
+# Channel est toujours renseigne.Folder est un parametre remseigbne a none la 1ere fois et contient votre parametre les fois suivantes.
+# Retourne un tableau **Ligne=[ channel, parameter pour prochain passage dans fonction, Titre pour menu, image pour menu, action effectuee quand clic sur menu]**
+#_Dernier parametre : 'folder' : retour dans meme fonctions, 'shows' prochain passage est dans "list_videos"
 def list_shows(channel,folder):
-    allshows = []
-    begin = time.time()        
-    
-    #launch 1 thread for each letter
-    threads = [];
-    for letter in lsLowercase:
-        thread = ShowLoadingThread(letter, allshows)
-        threads.append(thread)
-        thread.start()
-        
-    #wait for all threads to complete
-    for thread in threads:
-        thread.join()    
-    
-    #flatten list 
-    allshows = [val for sublist in allshows for val in sublist]
-    #list is populated by several threads in random order, order by name
-    allshows = sorted(allshows, key=itemgetter(2))
-    print("%s: took %ss to list all shows"%(channel, time.time()-begin))
-    
-    return allshows
-
-def list_videos(channel, emission_page):
-    print("%s: list videos  for emission %s"%(channel, emission_page))    
-    shows        = []    
-    ajaxcall     = get_search_url_for_emission(channel, emission_page)    
-    tempfile     = "%s_listvideos_%s.json"%(channel, emission_page)    
-    file_path    = utils.downloadCatalog(ajaxcall, tempfile, bypass_cache,{})    
-    raw          = open(file_path).read()
-    jsoncontent  = json.loads(raw)    
-    htmlcontent  = jsoncontent["content"].encode("UTF-8")
-    match        = emission_json_re.findall(str(htmlcontent), re.DOTALL)
-    parser       = HTMLParser();
-    if match:
-        for pic, url, name in match:
-            shows.append([channel,url,parser.unescape(name.decode("UTF-8")).encode("UTF-8") , pic, {},'play'] )
+    print("List show for folder " + folder)
+    if (folder == personnages_folder):
+        return load_personnages(channel)
     else:
-        print("no regexp match found in emission data !")            
-    #TODO load image
-    #TODO info labels    
-    return shows
-    
-def getVideoURL(channel,asset_id): 
-    print("INA get video URL: " + asset_id)    
-    url       = root_url + asset_id    
-    tempfile  = "%s_videourl_%s.html"%(channel, asset_id)    
-    file_path = utils.downloadCatalog(url, tempfile, False,{})
-    raw       = open(file_path).read()    
-    videolink = emission_url_re.search(raw).group(1)
-    print("%s: Video link for asset %s : %s"%(channel, asset_id, videolink))
-    return videolink
-    
-def load_emissions_for_letter(letter):
-    print("Loading emissions for letter  " + letter)    
-    start     = time.time()    
-    shows     = []        
-    tempfile  = "%s_emissionletter_%s.html"%(title[0], letter)    
-    #Load json result (ajax call from mobile app
-    file_path = utils.downloadCatalog(url_byletter+letter, tempfile, bypass_cache,{})    
-    raw       = open(file_path).read()    
-    #Treat as json, extract content field
-    jsoncontent = json.loads(raw)    
-    htmlcontent = jsoncontent["content"].encode("utf-8")    
-    #Apply regexp
-    match = emissions_re.findall(htmlcontent)    
-    #Convert regexp groups to array of values
-    if match:
-        for url, name, pic in match:
-            shows.append( [title[0], url, name , root_url + pic,'shows'] )    
-    print("took %ss to load emissions for letter %s"%(time.time()-start, letter))    
-    return shows  
- 
-def get_search_url_for_emission(channel, emission_page):
-    emission_page = root_url + emission_page    
-    tempfile      = "%s_details_%s.html "%(channel, emission_page)    
-    file_path     = utils.downloadCatalog(emission_page, tempfile, bypass_cache,{})    
-    raw           = open(file_path).read()    
-    result        = detail_re.search(raw)
-    return root_url + result.group(1) + '?' + result.group(2)
-    
-class ShowLoadingThread(threading.Thread):
-    
-    def __init__(self, letter, dest):
-        threading.Thread.__init__(self)
-        self.letter = letter
-        self.shows  = dest        
-        
-    def run(self):
-        results = load_emissions_for_letter(self.letter)
-        self.shows.append(results)
-     
-      
+        return load_categories(channel)
+
+
+def load_categories(channel):
+    categories = []
+
+    print("Loading categories")
+    soup = load_soup(categories_url, 'categories.html');
+
+    links = soup.select(categories_links_selector)
+    for link in links:
+        folder_path = link.get('href').encode('utf-8')
+        type = 'folder' if folder_path == personnages_folder else 'shows'
+        categories.append([channel, folder_path, link.getText().encode("utf-8"), '', type])
+
+    print("Categories: %s"%(categories))
+    return categories
+
+
+def load_personnages(channel):
+    personnages = []
+
+    print("Loading personnages")
+    soup = load_soup(personnages_url, 'personnages.html')
+
+    links = soup.select(personnages_link_selector)
+    for link in links:
+        path = root_url + link.get('href')
+        image = root_url + link.select_one('img').attrs['src']
+        title = link.attrs['title'].encode('utf-8')
+        personnages.append([channel, path, title, image, 'shows'])
+
+    return personnages
+
+
+
+#**list_videos(channel,show_URL)**: Fonctionne de maniere similaire.
+# Retourne un tableau **Ligne=[ channel, param pour passage dans fonction getvideoURL, Titre pour menu, image pour menu, infoLabels, 'play']**
+def list_videos(channel,show_name):
+    print('loading videos for ' + show_name)
+    videos = []
+
+    #current page is page 1
+    pagenumber = 1
+    folder_url = root_url + '/' + show_name
+    tempfile = '%s_%d'%(show_name, pagenumber)
+
+    #parse first page
+    print('Loading first page for %s from %s: '%(show_name, folder_url))
+    soup = load_soup(folder_url, tempfile)
+    parse_clips_page(soup, videos, channel)
+
+    #get last page link
+    pagelinks = soup.select('a.pageNum');
+    if (len(pagelinks) == 0):
+        #no page links (single result page), return first page results
+        return videos
+
+    #several pages to parse
+    #determine last page number and generate base pagination link
+    lastpage_link = pagelinks[-1]
+    lastpage_number = int(lastpage_link.getText().encode('utf-8'))
+    lastpage_href = root_url + lastpage_link.attrs['href'].encode('utf-8')
+
+    #remove trailing page number from string
+    base_page_href = re.sub( r"\/\d*$", '/', lastpage_href)
+
+    #parse pages 2 to last by adding page number to pagination links
+    for pagenumber in range(2, lastpage_number + 1):
+        page_url = '%s%d'%(base_page_href, pagenumber)
+        tempfile = '%s_%s'%(show_name, pagenumber)
+
+        print('Loading page %s for %s on %s...' % (pagenumber, show_name, page_url))
+        soup = load_soup(page_url, tempfile)
+        parse_clips_page(soup, videos, channel)
+
+    print("Found %d videos for %s"%(len(videos), show_name))
+    return videos
+
+
+def parse_clips_page(soup, results, channel):
+    for item in soup.select(clip_selector):
+        link = root_url + item.select_one('a').attrs['href']
+        image = item.select_one('img')
+        imagelink = root_url + '/' + image.attrs['src']
+        title = image.attrs['alt'].encode('utf-8')
+        results.append([channel, link, title, imagelink, '', 'play'])
+
+
+#getVideoURL(channel,video_URL)**: Retourne l'URL qui devra etre lu par KODI
+def getVideoURL(channel,video_URL):
+    tempfile = video_URL
+    print("VIDEO URL : " + video_URL)
+    file_path = utils.downloadCatalog(video_URL, tempfile, bypass_cache, {})
+    html = open(file_path).read()
+    videoId  = video_id_re.search(html).group(1)
+    result =  'https://youtu.be/' + videoId
+    print("RESULT : " + result)
+    return result;
