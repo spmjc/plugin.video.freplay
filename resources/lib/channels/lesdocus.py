f94d6167646c6f02e638b5e16e147500c8c4265d
SPM <capestanj@gmail.com>
2017-11-06 14:52:04 -0500

    les docus
    Missing Youtube provider

--- resources/lib/channels/lesdocus.py
+++ resources/lib/channels/lesdocus.py
@@ -1,4 +1,5 @@
-#-*- coding: utf-8 -*-
+#-*- coding: utf-8 -*-              
+import json       
 import urllib2
 import re
 import CommonFunctions
@@ -10,47 +11,58 @@ title=['Les Docus']
 img=['lesdocus']
 readyForUse=True
 
+categ='{"categories":[{"id":"","name":"Accueil","sub":[]},{"id":"arts","name":"Arts","sub":[{"id":"architecture","name":"Architecture"},{"id":"cinema","name":"Cinema"},{"id":"dessin","name":"Dessin"},{"id":"litterature","name":"Litterature"},{"id":"musique","name":"Musique"},{"id":"peinture","name":"Peinture"},{"id":"sculpture","name":"Sculpture"}]},{"id":"histoire","name":"Histoire","sub":[{"id":"prehistoire","name":"Prehistoire"},{"id":"antiquite","name":"Antiquite"},{"id":"moyen-age","name":"Moyen Age"},{"id":"temps-modernes","name":"Temps Modernes"},{"id":"temps-revolutionnaires","name":"Temps revolutionnaires"},{"id":"19eme-siecle","name":"19eme siecle"},{"id":"20eme-siecle","name":"20eme siecle"},{"id":"epoque-contemporaine","name":"Epoque contemporaine"}]},{"id":"societe","name":"Societe","sub":[{"id":"argent","name":"Argent"},{"id":"monde","name":"Monde"},{"id":"politique","name":"Politique"},{"id":"sexualite","name":"Sexualite"},{"id":"social","name":"Social"}]},{"id":"sciences","name":"Sciences","sub":[{"id":"astronomie","name":"Astronomie"},{"id":"ecologie","name":"Ecologie"},{"id":"economie","name":"Economie"},{"id":"genetique","name":"Genetique"},{"id":"geographie","name":"Geographie"},{"id":"geologie","name":"Geologie"},{"id":"mathematique","name":"Mathematique"},{"id":"medecine","name":"Medecine"},{"id":"physique","name":"Physique"},{"id":"psychologie","name":"Psychologie"}]},{"id":"technologie","name":"Technologie","sub":[{"id":"aviation","name":"Aviation"},{"id":"informatique","name":"Informatique"},{"id":"marine","name":"Marine"},{"id":"telephonie","name":"Telephonie"}]},{"id":"paranormal","name":"Paranormal","sub":[{"id":"fantomes-et-esprits","name":"Fantomes et Esprits"},{"id":"ovnis-et-extraterrestres","name":"Ovni et Extraterrestres"},{"id":"cryptozoologie","name":"Cryptozoologie"},{"id":"mysteres-et-legendes","name":"Mysteres et Legendes"},{"id":"divers","name":"Divers"}]},{"id":"autres","name":"Autres","sub":[{"id":"animaux","name":"Animaux"},{"id":"gastronomie","name":"Gastronomie"},{"id":"jeux-video","name":"Jeux video"},{"id":"loisirs","name":"Loisirs"},{"id":"metiers","name":"Metiers"},{"id":"militaire","name":"Militaire"},{"id":"nature","name":"Nature"},{"id":"policier","name":"Policier"},{"id":"religion","name":"Religion"},{"id":"sante","name":"Sante"},{"id":"sport","name":"Sport"},{"id":"voyage","name":"Voyage"}]}]}'
+
 def list_shows(channel,folder):
   shows=[]
-  d = dict()         
-  filePath=utils.downloadCatalog('http://www.les-docus.com/','lesdocus.html',False,{})
-  html=open(filePath).read().replace('\xe9', 'e').replace('\xe0', 'a').replace('\n', ' ').replace('\r', '')
-  if folder=='none' :
-    
-    match = re.compile(r'<li id="(.*?)" class="(.*?)menu-item-has-children has-children(.*?)"><a href="(.*?)">(.*?)</a>',re.DOTALL).findall(html)
-    for empty1,empty2,empty3,url,title in match:
-      if title not in d:
-        shows.append( [channel,url, title ,'' ,'folder'] )
-        d[title] = title
-  if folder !='none':
   
-    match = re.compile(r'<li id="(.*?)" class="(.*?)"><a href="(.*?)">(.*?)</a></li>',re.DOTALL).findall(html)
-    for empty1,empty2,url,title in match:
-      if folder in url and folder != url and url not in d:
-        shows.append( [channel,url, title ,'' ,'shows'] )
-        d[url] = url
+  jsonParser = json.loads(categ)
+    
+  if folder=='none' :
+    for c in jsonParser['categories']:
+      if c['id']=='':
+        shows.append( [channel,c['id'] + '|1', c['name'] ,'' ,'shows'] )
+      else:
+        shows.append( [channel,c['id'], c['name'],'' ,'folder'] )
+    
+  if folder !='none':  
+    for c in jsonParser['categories']:
+      if c['id']==folder:
+        for s in c['sub']:
+          shows.append( [channel,'/' + c['id'] + '/' + s['id'] + '|1', s['name'],'' ,'shows'] )
     
   return shows
   
-def list_videos(channel,url):     
+def list_videos(channel,folder):     
   videos=[] 
-  
-  filePath=utils.downloadCatalog(url,'lesdocuslist.html',True,{})
+    
+  cat,page=folder.split('|')
+  filePath=utils.downloadCatalog('http://www.les-docus.com%s/page/%s/' % (cat,page),'lesdocuslist.html',True,{})
   html=open(filePath).read().replace('\xe9', 'e').replace('\xe0', 'a').replace('\n', ' ').replace('\r', '')
-  print html
-  print 'herrrrrre'
+  
+  match = re.compile(r'<a class="(.*?) page-numbers"',re.DOTALL).findall(html)
+  prev=False
+  next=False
+  for item in match:
+    prev=('prev' in item or prev)
+    next=('next' in item or next)
+  
+  if prev:
+    videos.append( [channel,cat + '|' + str(int(page)-1), '<<Page Precedente' ,'',{}  ,'shows'] )
+              
   match = re.compile(r'<div class="post-header"> <a href="(.*?)" title="(.*?)">',re.DOTALL).findall(html)
-  print len(match)
   for url,title in match:   
     infoLabels={ "Title": title}
-    videos.append( [channel, url , title , '',infoLabels,'play'] ) 
+    videos.append( [channel, url , title , '',infoLabels,'play'] )
+  
+  if next:
+    videos.append( [channel,cat + '|' + str(int(page)+1), 'Page Suivante>>' ,'',{}  ,'shows'] ) 
   
   return videos
   
 def getVideoURL(channel,url):
   html=utils.get_webcontent(url).replace('\xe9', 'e').replace('\xe0', 'a').replace('\n', ' ').replace('\r', '')  
   url=re.findall('<noscript><iframe(.*?)src="(.*?)"', html)[0][1]
-  print url
   if 'vimeo' in url:
     return utils.getVimeoURL(url)
   else:
